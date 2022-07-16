a492047a564a65b85bc25079b32a2a2631542747
SeoulP <55219779+SeoulP@users.noreply.github.com>
2022-07-16 11:55:14 -0500

    Oven Functionality

--- GMTK-2022/Assets/Scripts/Oven.cs
+++ GMTK-2022/Assets/Scripts/Oven.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Oven : MonoBehaviour
 {
+    [SerializeField]
+    int pizzaCookSpeed = 5;
+    [SerializeField]
+    GameObject player;
+    PickUpTestScript chefPickup;
+    GameObject item;
     Pizza pizza;
     bool isFullyCooked = false;
 
@@ -12,14 +18,47 @@ public class Oven : MonoBehaviour
     void Start()
     {
         alert = GetComponent<AudioSource>();
+        chefPickup = player.GetComponent<PickUpTestScript>();
     }
     
     void Update()
     {
+        // Get player distance
+        float playerDistance = (player.transform.position - this.transform.position).magnitude;
+        Debug.Log("Player Distance: " + playerDistance);
+
+        // if player is within range and presses space, take item
+        if(playerDistance <= 5 && Input.GetKeyDown(KeyCode.E) && chefPickup.itemHolding)
+        {
+            // check player for held item
+            this.item = chefPickup.itemHolding;
+            chefPickup.itemHolding = null;
+            Debug.Log("We have an item in the oven!");
+        }
+
+        // Check if item is pizza
+        if(item)
+        {
+            if (item.GetComponent<Pizza>())
+            {
+                this.pizza = this.item.GetComponent<Pizza>();
+                Debug.Log("THE ITEM IS A PIZZA TOO!");
+            }
+            else
+            {
+                // destroy item
+                this.item = null;
+                Debug.Log("Ope, it wasn't a pizza. You burned it.");
+            }
+        }
+
+
+        // If item is pizza, cook pizza
         if (pizza)
         {
-            pizza.cooked = 1 * Time.deltaTime;
+            pizza.cooked += pizzaCookSpeed * Time.deltaTime;
 
+            Debug.Log("Pizza is being cooked, boys! " + pizza.cooked);
             if(pizza.cooked <= 100)
             {
                 alert.Play();
