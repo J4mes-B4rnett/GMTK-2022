33abca858682a741a489aaa9eca15ff61ba50f63
James Barnett <54456440+J4mes-B4rnett@users.noreply.github.com>
2022-07-16 09:33:12 +0100

    Setup animation controller + animations + improved the order generator

--- GMTK-2022/Assets/Scripts/Order_Generation.cs
+++ GMTK-2022/Assets/Scripts/Order_Generation.cs
@@ -15,21 +15,21 @@ public class Order_Generation : MonoBehaviour
         {
             case 1:
             {
-                string fileName = @"Assets/Scripts/toppings.txt";
+                string fileName = @"Assets/Scripts/Ingredients/toppings.txt";
                 IEnumerable<string> lines = File.ReadLines(fileName);
                 Console.WriteLine(String.Join(Environment.NewLine, lines));
                 return lines.ElementAt((int)Random.Range(0, lines.Count()));
             }
             case 2:
             {
-                string fileName = @"Assets/Scripts/sauces.txt";
+                string fileName = @"Assets/Scripts/Ingredients/sauces.txt";
                 IEnumerable<string> lines = File.ReadLines(fileName);
                 Console.WriteLine(String.Join(Environment.NewLine, lines));
                 return lines.ElementAt((int)Random.Range(0, lines.Count()));
             }
             case 3:
             {
-                string fileName = @"Assets/Scripts/attributes.txt";
+                string fileName = @"Assets/Scripts/Ingredients/attributes.txt";
                 IEnumerable<string> lines = File.ReadLines(fileName);
                 return lines.ElementAt((int)Random.Range(0, lines.Count()));
             }
@@ -38,31 +38,60 @@ public class Order_Generation : MonoBehaviour
         }
     }
     
-    string OrderGenerator(int ingredientCount)
+    List<string> OrderGenerator(int ingredientCount)
     {
-        string orderString = "I would like a pizza with ";
+        List<string> ingredients;
+        ingredients = new List<String>();
+        ingredients.Add((ingredientCount).ToString());
+        var newIngredient = "";
+        
         if (ingredientCount > 1) // When there are multiple ingredients
         {
             for (var i = 0; i < ingredientCount; i++)
             {
-                
+                newIngredient = RandomIngredient(1);
+                ingredients.Add(newIngredient);
             }
         }
         else
         {
-            orderString += RandomIngredient(1);
+            newIngredient = RandomIngredient(1);
+            ingredients.Add(newIngredient);
         }
         
-        orderString += ", I also want ";
-        orderString += RandomIngredient(2);
-        orderString += ", and please ";
-        orderString += RandomIngredient(3);
-        orderString += "!";
-        return orderString;
+        newIngredient = RandomIngredient(2);
+        ingredients.Add(newIngredient);
+        
+        newIngredient = RandomIngredient(3);
+        ingredients.Add(newIngredient); ;
+        return ingredients;
+    }
+
+    string OrderString(List<string> ingredients)
+    {
+        string order = "Hello, I would like ";
+        if (Int32.Parse(ingredients[0]) > 1)
+        {
+            for (var i = 0; i < Int32.Parse(ingredients[0]); i++)
+            {
+                order += ingredients[i + 1] + ", ";
+            }
+        }
+        else
+        {
+            order += ingredients[1];
+        }
+
+        order += "& ";
+        order += ingredients[Int32.Parse(ingredients[0]) + 1];
+        order += " - and please ";
+        order += ingredients[Int32.Parse(ingredients[0]) + 2];
+        order += "!";
+        return order;
     }
 
     public void Start()
     {
-        print(OrderGenerator(1));
+        print(OrderString(OrderGenerator(3)));
     }
 }
