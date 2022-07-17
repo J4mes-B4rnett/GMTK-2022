d4422bae972a0caa64c98b4e38b69c42a52fe955
James Barnett <54456440+J4mes-B4rnett@users.noreply.github.com>
2022-07-17 17:33:00 +0100

    Revert "Updated Game Over"
    This reverts commit e8d2c16dcd9de1341b605380c2f2dbe01ac3ebec.

--- GMTK-2022/Assets/Scripts/OrderComplete.cs
+++ GMTK-2022/Assets/Scripts/OrderComplete.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class OrderComplete : MonoBehaviour
 {
@@ -13,25 +12,7 @@ public class OrderComplete : MonoBehaviour
     Pizza pizza;
     [SerializeField] int pizzaToppingPenalty = 5;
     [SerializeField] int pizzaSaucePenalty = 10;
-<<<<<<< Updated upstream
     [SerializeField] int pizzaBoxPenalty = 30;
-=======
-    [SerializeField] int pizzaBoxPenalty = 50;
-
-    public int totalPizzas = 0;
-    public int totalSuccessfulPizzas = 0;
-    public int totalPizzasCompleted = 0;
-    public int totalRentPayed = 0;
-    public int totalDays = 0;
-    public int finalDifficulty = 0;
->>>>>>> Stashed changes
-
-    [SerializeField] private TextMeshProUGUI textTP;
-    [SerializeField] private TextMeshProUGUI textSP;
-    [SerializeField] private TextMeshProUGUI textPC;
-    [SerializeField] private TextMeshProUGUI textRP;
-    [SerializeField] private TextMeshProUGUI textTD;
-    [SerializeField] private TextMeshProUGUI textFD;
 
     [SerializeField] float interactionDistance = 1f;
 
@@ -52,13 +33,6 @@ public class OrderComplete : MonoBehaviour
         // Pull in ingredients on Pizza
         // Compare the two
 
-        textTP.text = totalPizzas.ToString();
-        textSP.text = totalSuccessfulPizzas.ToString();
-        textPC.text = (totalPizzasCompleted-totalSuccessfulPizzas).ToString();
-        textRP.text = "£" + totalRentPayed.ToString();
-        textTD.text = totalDays.ToString();
-        textFD.text = finalDifficulty.ToString();
-        
         float playerDistance = (player.transform.position - this.transform.position).magnitude;
 
         if (playerDistance <= interactionDistance && Input.GetKeyDown(KeyCode.E) && chefPickup.heldObject && chefPickup.heldObject.GetComponent<Pizza>())
@@ -84,26 +58,7 @@ public class OrderComplete : MonoBehaviour
                 pizza.rating -= pizzaBoxPenalty;
             }
 
-<<<<<<< Updated upstream
             if (isDebugging)
-=======
-
-            if(pizza.rating >= 80)
-            {
-                totalSuccessfulPizzas++;
-            }
-            totalPizzasCompleted++;
-
-            totalRentPayed = GameObject.FindObjectOfType<Rent_Manager>().GetComponent<Rent_Manager>().totalRentPayed;
-            totalDays = GameObject.FindObjectOfType<Rent_Manager>().GetComponent<Rent_Manager>().day;
-            finalDifficulty = GameObject.FindObjectOfType<Rent_Manager>().GetComponent<Rent_Manager>().difficulty;
-
-
-            GameObject.FindObjectOfType<Rent_Manager>().GetComponent<Rent_Manager>().rentPayed += pizza.rating * 2;
-            GameObject.FindObjectOfType<Rent_Manager>().GetComponent<Rent_Manager>().NextPizza();
-            
-                    if (isDebugging)
->>>>>>> Stashed changes
                 Debug.Log("Your pizza's score was " + pizza.rating + "!");
 
             GameObject temp = chefPickup.heldObject;
