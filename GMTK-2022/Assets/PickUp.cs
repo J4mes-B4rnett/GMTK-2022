b43f72c2f66e8608b4fa3d3e67e1ed2fb7846fc0
James Barnett <54456440+J4mes-B4rnett@users.noreply.github.com>
2022-07-17 10:20:52 +0100

    NPC generation

---
+++ GMTK-2022/Assets/PickUp.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PickUp : MonoBehaviour
+{
+    public Transform holdPoint;
+    public float zoneSize = 5f;
+    
+
+    void Update()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            
+        }
+    }
+}
