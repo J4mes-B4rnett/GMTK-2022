de2b8fe11b1368b309fc2e5f0765d08bb112e0bf
SeoulP <55219779+SeoulP@users.noreply.github.com>
2022-07-17 06:43:54 -0500

    Fridge Works boys!

--- GMTK-2022/Assets/Scripts/Fridge.cs
+++ GMTK-2022/Assets/Scripts/Fridge.cs
@@ -16,13 +16,23 @@ public class Fridge : MonoBehaviour
     GameObject fridge;
 
     [SerializeField]
-    List<Transform> sauceSlots;
+    GameObject selector;
+
+    [SerializeField]
+    List<GameObject> sauceSlots;
+
+    [SerializeField]
+    List<GameObject> saucePrefabs;
+
+    int sauceIndex = 0;
 
     private List<GameObject> sauceObjects = new List<GameObject>();
     private bool sauceInitiated = false;
 
     [SerializeField]
     List<GameObject> sauces;
+
+    private float timer = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -32,10 +42,11 @@ public class Fridge : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timer += 1 * Time.deltaTime;
         // Get player distance
         float playerDistance = (player.transform.position - this.transform.position).magnitude;
 
-        if (playerDistance <= interactionDistance && Input.GetKeyDown(KeyCode.E) && !chefPickup.heldObject)
+        if (playerDistance <= interactionDistance && Input.GetKeyDown(KeyCode.E) && !chefPickup.heldObject && !fridge.active)
         {
             if(isDebugging)
             {
@@ -45,6 +56,9 @@ public class Fridge : MonoBehaviour
             fridge.SetActive(true);
             playerSpeed = player.GetComponent<Chef_Controller>().moveSpeed;
             player.GetComponent<Chef_Controller>().moveSpeed = 0;
+            selector.transform.position = sauceSlots[0].transform.position;
+            sauceIndex = 0;
+
             SetSauces();
 
 
@@ -54,47 +68,97 @@ public class Fridge : MonoBehaviour
         {
             fridge.SetActive(false);
             player.GetComponent<Chef_Controller>().moveSpeed = playerSpeed;
-            foreach (GameObject sauce in sauceObjects)
+            ClearSauces();
+        }
+
+
+        if(Input.GetKeyDown(KeyCode.D) && sauceInitiated)
+        {
+            // Move to next item
+            sauceIndex++;
+            if(sauceIndex > sauceObjects.Count - 1)
             {
-                if (isDebugging)
-                    Debug.Log(sauce.name + " destroyed!");
-                GameObject.Destroy(sauce);
+                sauceIndex = 0;
             }
-            sauceInitiated = false;
+
+            selector.transform.position = sauceSlots[sauceIndex].transform.position;
+        }
+        else if (Input.GetKeyDown(KeyCode.A) && sauceInitiated)
+        {
+            // Move to previous item
+            sauceIndex--;
+            if (sauceIndex < 0)
+            {
+                sauceIndex = sauceObjects.Count - 1;
+            }
+            selector.transform.position = sauceSlots[sauceIndex].transform.position;
 
         }
+        else if (Input.GetKeyDown(KeyCode.E) && timer > .5f && sauceInitiated)
+        {
+
+            string sauceName = "";
+            foreach(GameObject sauce in sauceObjects)
+            {
+                // if sauce in slot matches sauce in sauceObject, grab sauce
+
+
+                if(sauceSlots[sauceIndex].transform.GetChild(0).name == sauce.name)
+                {
+                    sauceName = sauce.name;
+                    Debug.Log("Sauce: " + sauceName);
+                    // Gimme da sauce
+                }
+            }
+
+            foreach (GameObject prefab in saucePrefabs)
+            {
+                if (sauceName.Contains(prefab.name))
+                {
+                    chefPickup.SetNewPickup(Instantiate(prefab));
+                }
+            }
+
+            ClearSauces();
+
+            fridge.SetActive(false);
+            player.GetComponent<Chef_Controller>().moveSpeed = playerSpeed;
+            ClearSauces();
+        }
+
 
     }
 
     private void SetSauces()
     {
+        timer = 0;
         List<GameObject> tempSauces = new List<GameObject>();
         sauceObjects = new List<GameObject>();
         tempSauces.AddRange(sauces);
         sauceInitiated = true;
 
-        foreach (Transform slot in sauceSlots)
+        foreach (GameObject slot in sauceSlots)
         {
             
             int sauceIndex = Random.Range(0, tempSauces.Count);
 
-            if (isDebugging)
-            {
-                Debug.Log("tempSauces Size: " + tempSauces.Count);
-            }
-                
             GameObject sauce = Instantiate(tempSauces[sauceIndex]);
 
-            if(isDebugging)
-            Debug.Log(sauce.name + " created!");
-
             sauceObjects.Add(sauce);
-            sauce.transform.position = slot.position;
-            sauce.gameObject.transform.parent = slot;
+            sauce.transform.position = slot.transform.position;
+            sauce.gameObject.transform.SetParent(slot.transform);
             tempSauces.RemoveAt(sauceIndex);
         }
     }
 
+    private void ClearSauces()
+    {
+        foreach (GameObject sauce in sauceObjects)
+        {
+            GameObject.Destroy(sauce);
+        }
+        sauceInitiated = false;
+    }
     void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
