e8d2c16dcd9de1341b605380c2f2dbe01ac3ebec
James Barnett <54456440+J4mes-B4rnett@users.noreply.github.com>
2022-07-17 17:27:47 +0100

    Updated Game Over

--- GMTK-2022/Assets/TimerDecrease.cs
+++ GMTK-2022/Assets/TimerDecrease.cs
@@ -11,7 +11,15 @@ public class TimerDecrease : MonoBehaviour
     public float CurrentTime;
     public bool TimeIsDone = false;
 
+<<<<<<< Updated upstream:GMTK-2022/Assets/TimerDecrease.cs
     // Start is called before the first frame update
+=======
+    void Awake()
+    {
+        Time.timeScale = 1f;
+    }
+    
+>>>>>>> Stashed changes:GMTK-2022/Assets/Scripts/TimerDecrease.cs
     void Start()
     {
         TimeBarImage = GetComponent<Image>();
@@ -23,8 +31,17 @@ public class TimerDecrease : MonoBehaviour
     {
       if (CurrentTime > 0)
       {
+<<<<<<< Updated upstream:GMTK-2022/Assets/TimerDecrease.cs
             CurrentTime -= 1 * Time.deltaTime;
             TimeBarImage.fillAmount = CurrentTime / MaxTime;
+=======
+          CurrentTime -= 1 * Time.deltaTime;
+          TimeBarImage.fillAmount = CurrentTime / MaxTime;
+      }
+      else
+      {
+          GameObject.FindObjectOfType<GameOverManager>().GetComponent<GameOverManager>().GameOver();
+>>>>>>> Stashed changes:GMTK-2022/Assets/Scripts/TimerDecrease.cs
       }
 
     }
